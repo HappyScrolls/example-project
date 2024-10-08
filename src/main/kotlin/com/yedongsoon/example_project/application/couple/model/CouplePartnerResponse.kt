529fbda8e807d10e5c96b50b04ce2f70d45a6fd9
Yeji <mnbvcxzyj@gmail.com>
2024-10-08 15:27:04 +0900

    fix: 공통일정 response 수정

--- src/main/kotlin/com/yedongsoon/example_project/application/couple/model/CouplePartnerResponse.kt
+++ src/main/kotlin/com/yedongsoon/example_project/application/couple/model/CouplePartnerResponse.kt
@@ -4,7 +4,7 @@ data class CouplePartnerResponse(
         val no: Int,
         val accountId: String,
         val name: String,
-        val email: String,
+        val email: String? = null,
         val profilePhoto: String? = null,
         val birthDate: String?,
         val mobileNo: String?,
