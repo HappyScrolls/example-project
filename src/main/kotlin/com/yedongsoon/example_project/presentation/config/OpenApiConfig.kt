7e1722d18817c27f4451dac9aea363f00a744156
chs98412 <chs98412@gmail.com>
2024-11-09 22:28:24 +0900

    swagger setting

--- src/main/kotlin/com/yedongsoon/example_project/presentation/config/OpenApiConfig.kt
+++ src/main/kotlin/com/yedongsoon/example_project/presentation/config/OpenApiConfig.kt
@@ -1,31 +1,13 @@
-//package com.yedongsoon.example_project.presentation.config
-//
-//import io.swagger.v3.oas.models.OpenAPI
-//import io.swagger.v3.oas.models.info.Info
-//import org.springdoc.core.models.GroupedOpenApi
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//
-//@Configuration
-//class OpenApiConfig {
-//
-//    @Bean
-//    fun publicApi(): GroupedOpenApi {
-//        return GroupedOpenApi.builder()
-//                .group("v1")
-//                .pathsToMatch("/**")
-//                .packagesToScan("com.yedongsoon.example_project")
-//                .build()
-//    }
-//
-//    @Bean
-//    fun customOpenAPI(): OpenAPI {
-//        return OpenAPI()
-//                .info(
-//                        Info()
-//                                .title("Schedule API")
-//                                .version("v1.0")
-//                                .description("API 명세서입니다.")
-//                )
-//    }
-//}
+package com.yedongsoon.example_project.presentation.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.ResourceHandlerRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class OpenApiConfig : WebFluxConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations("file:build/api-spec/")
+    }
+}
